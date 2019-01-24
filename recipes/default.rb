9d0c82e8b9b1d67e5389b3b40a4135f535df1396
Sebastien Chartrand <sebastien.chartrand@fujitsu.com>
2019-01-24 10:54:52 -0500

    Added Aeysha fixed

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,9 @@
-# Cookbook:: fai_chef_client_updater
-# Recipe:: default
-#
 # Copyright:: 2018, The Authors, All Rights Reserved.
-chef_client_updater 'Install latest chefdk' do
-  product_name 'chefdk'
-  only_if { node['packages'].keys.include? 'chefdk' }
-end
-
-chef_client_updater 'Install latest Chef client' do
-  only_if { node['packages'].keys.include? 'chef' }
+chef_client_updater 'Install latest' do
+  case node['os']
+  when 'windows'
+    post_install_action 'exec'
+  else
+    post_install_action 'kill'
+  end
 end
